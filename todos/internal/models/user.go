7a0418666d62516dc593140707ce989ab01b8e16
Shiva <kumarshiva0707@gmail.com>
2026-02-08 01:13:43 +0530

    added - instead of the password so it can omit the pw before sending it to the client

--- todos/internal/models/user.go
+++ todos/internal/models/user.go
@@ -5,7 +5,9 @@ import "time"
 type User struct {
 	ID        string    `json:"id" db:"id"`
 	Email     string    `json:"email" db:"email"`
-	Password  string    `json:"password" db:"password"`
+	Password  string    `json:"-" db:"password"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
+
+// - omits the password before going to frontend
